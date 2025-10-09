74bbccaf7668393f45da1c6222748e2565670c22
Alex Burs <alexd.burs@gmail.com>
2025-10-08 23:09:00 -0300

    Agregado: Lógica Mack

--- aaa.r
+++ aaa.r
@@ -1,24 +1,5 @@
 library(ChainLadder)
 
-development <- function(RAA) {
-    n <- ncol(RAA)
-    dev_promedio_simple <- sapply(1:(n - 1), function(i) {
-        sum(RAA[c(1:(n - i)), i + 1]) / sum(RAA[c(1:(n - i)), i])
-    })
-
-    dev_promedio_ponderado <- sapply(1:(n - 1), function(i) {
-        mean(RAA[c(1:(n - i)), i + 1] / RAA[c(1:(n - i)), i])
-    })
-
-    link_ratio <- RAA[, 2:n] / RAA[, 1:(n - 1)]
-
-    list(
-        dev_promedio_simple = dev_promedio_simple,
-        dev_promedio_ponderado = dev_promedio_ponderado,
-        link_ratios = link_ratio
-    )
-}
-
 development_acum <- function(
     link_ratios,
     tri_development,
@@ -64,7 +45,6 @@ development_acum <- function(
 
     link_ratios_acum2 <- link_ratios
     for (i in 2:(link_ratios_col)) {
-        print(link_ratios_col - i + 1)
         link_ratios_acum2[1:(link_ratios_col - i + 1), i] <- link_ratios_acum2[
             1:(link_ratios_col - i + 1),
             i - 1
@@ -79,6 +59,38 @@ development_acum <- function(
     ))
 }
 
+cl_rgaa <- function(triang, tail) {
+    # Calculo linkratios
+    linkratios <- c(attr(ata(triang), "vwtd"), tail)
+
+    #Calculo triangulo entero
+    n <- ncol(triang)
+    triang_full <- cbind(triang, Ult = rep(0, nrow(triang)))
+
+    for (k in 1:n) {
+        triang_full[(n - k + 1):n, k + 1] <- triang_full[(n - k + 1):n, k] *
+            linkratios[k]
+    }
+
+    #Calculo tabla de resumen
+    FDA <- rev(cumprod(rev(linkratios)))
+    names(FDA) <- colnames(triang)
+    diagonal_actual <- getLatestCumulative(triang)
+    ultimate <- diagonal_actual * rev(FDA) #
+    resumen <- data.frame(diagonal_actual, FDA = rev(FDA), ultimate)
+    resumen <- rbind(
+        resumen,
+        data.frame(
+            diagonal_actual = sum(diagonal_actual),
+            FDA = NA,
+            ultimate = sum(ultimate),
+            row.names = "Total"
+        )
+    )
+    list(resumen = resumen, triang_full = triang_full)
+}
+
+
 # Cargar el archivo
 file_path <- ".\\sample\\ssn_20192020_desarrollo_siniestros_automotores.csv"
 triangle_long <- read.csv(
@@ -91,6 +103,7 @@ triangle_long <- read.csv(
 
 # Armar los datasets
 tri_RC <- subset(triangle_long, Ramo %in% "RC")
+tri_RC$origin <- as.integer(substr(tri_RC$origin, 1, 4))
 tri_RC.incr <- as.triangle(
     tri_RC,
     origin = "origin",
@@ -121,10 +134,9 @@ plot(tri_RC.incr_cum)
 
 
 RAA <- tri_RC.incr_cum
-dev_list <- development(RAA)
-tri_RC.incr_cum.dev_prom_smple <- dev_list$dev_promedio_simple
-tri_RC.incr_cum.dev_prom_pond <- dev_list$dev_promedio_ponderado
-tri_RC.incr_cum.link_ratios <- dev_list$link_ratios
+tri_RC.incr_cum.dev_prom_smple <- c(attr(ata(RAA), "vwtd")) # --> "vwtd" para E(Xi+1)/E(Xi+1)
+tri_RC.incr_cum.dev_prom_pond <- c(attr(ata(RAA), "smpl")) # --> "smpl" para E(xi+1/xi)
+tri_RC.incr_cum.link_ratios <- RAA[, 2:ncol(RAA)] / RAA[, 1:(ncol(RAA) - 1)]
 
 dev_acum_list <- development_acum(
     tri_RC.incr_cum.link_ratios,
@@ -144,3 +156,11 @@ plot(
     lattice = FALSE
 )
 plot(as.triangle(as.matrix(tri_RC.incr_cum.full_link_rt3)), lattice = TRUE)
+
+
+tri_RC.incr_cum.rgaa <- cl_rgaa(triang = tri_RC.incr_cum, tail = 1.05)
+
+mack <- MackChainLadder(tri_RC.incr_cum, est.sigma = "Mack")
+
+plot(mack)
+plot(mack, lattice = TRUE)
